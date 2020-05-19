a0baad493137bba5ee594fbbc0d1380fbd5f5317
Flavio <fla_edu@live.com>
2020-05-18 23:42:15 -0300

    Menu com QRCode

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles'
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles'
 
 export default function Menu() {
     return (
         <Container>
+            <Code>
+                <QRCode
+                    value="https://github.com/fla-edu"
+                    size={80}
+                    backgroundColor="#FFF"
+                    color="#8B10AE"
+                />
+            </Code>
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF"/>
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF"/>
+                    <NavText>Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF"/>
+                    <NavText>Configurar cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF"/>
+                    <NavText>Configurações do app</NavText>
+                </NavItem>
+            </Nav>
 
+            <SignOutButton onPress={() => {}} >
+                <SignOutButtonText>SAIR DA CONTA</SignOutButtonText>
+            </SignOutButton>
         </Container>
     );
 }
